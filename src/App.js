534b011dd4281bae9fd2f28bbf7b5f2c5270e8c2
Han-kiyeon <kylin3100@naver.com>
2020-03-11 21:46:06 +0900

    init

--- src/App.js
+++ src/App.js
@@ -2,25 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const a = () => {
-  return '2';
-};
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello React</p>
       </header>
     </div>
   );
